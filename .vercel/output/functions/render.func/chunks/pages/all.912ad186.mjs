d6be2c8fd9f206806c04cb90569a301f31972401
Thuan Pham <thuanowa@gmail.com>
2023-05-11 22:25:01 +0700

    chore

--- .vercel/output/functions/render.func/chunks/pages/all.912ad186.mjs
+++ .vercel/output/functions/render.func/chunks/pages/all.912ad186.mjs
@@ -18,11 +18,32 @@ const $$Index = createComponent(async ($$result, $$props, $$slots) => {
     <h1 class="astro-J7PV25F6">Uncopyright</h1>
     <p class="astro-J7PV25F6">All my contents, are uncopyright.</p>
     <p class="astro-J7PV25F6">
-      No need to email me for permission — use my content whatever you want!
-      Email it, share it, and reprint it with or without credit. Change it
-      around, put in a bunch of swear words, and attribute them to me. It’s OK.
+      No need to <a href="mailto:thuanowa@gmail.com" class="astro-J7PV25F6">email me</a> for permission
+      — use my content whatever you want! Email it, share it, and reprint it with
+      or without credit. Change it around, put in a bunch of swear words, and attribute
+      them to me. It’s OK.
     </p>
     <p class="astro-J7PV25F6">Attribution is appreciated but not required.</p>
+    <p class="astro-J7PV25F6">List of contents using uncopyright:</p>
+    <ul class="astro-J7PV25F6">
+      <li class="astro-J7PV25F6">
+        Contents in domain name and subdomain of <a href="https://thuanowa.com" class="astro-J7PV25F6">thuanowa.com</a>
+      </li>
+      <li class="astro-J7PV25F6">
+        My code, example my GitHub <a href="https://github.com/thuanowa" class="astro-J7PV25F6">github.com/thuaowa</a>
+      </li>
+      <li class="astro-J7PV25F6">
+        My videos on Youtube <a href="https://youtube.com/@thuanowa" class="astro-J7PV25F6">
+          youtube.com/@thuanowa</a>
+      </li>
+      <li class="astro-J7PV25F6">
+        My photos on Google Photos <a href="https://l.thuanowa.com/photos" class="astro-J7PV25F6">
+          l.thuanowa.com/photos</a>
+      </li>
+    </ul>
+    <q class="astro-J7PV25F6">Feel free to add your list of uncopyright content in here. <br class="astro-J7PV25F6">
+      <a href="mailto:thuanowa@gmail.com" class="astro-J7PV25F6">Ask me</a> or just create pull request
+      on this repo <a href="https://github.com/thuanowa/uncopyright" class="astro-J7PV25F6">github.com/thuanowa/uncopyright</a></q>
   </body></html>`;
 }, "/home/master/repos/thuanowa/uncopyright/src/pages/index.astro");
 
